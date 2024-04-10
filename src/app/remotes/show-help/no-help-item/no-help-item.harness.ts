9380b81dcae120fbca8ce379ed48aa868a597950
markuczy <martin.kuczynski@capgemini.com>
2024-04-10 12:03:51 +0200

    fix: comment out harnesses until tests are ready

--- src/app/remotes/show-help/no-help-item/no-help-item.harness.ts
+++ src/app/remotes/show-help/no-help-item/no-help-item.harness.ts
@@ -1,22 +1,22 @@
-import { ComponentHarness } from '@angular/cdk/testing'
-import { SpanHarness } from '@onecx/angular-testing'
+// import { ComponentHarness } from '@angular/cdk/testing'
+// import { SpanHarness } from '@onecx/angular-testing'
 
-export class NoHelpItemHarness extends ComponentHarness {
-  static hostSelector = 'app-ocx-no-help-item'
+// export class NoHelpItemHarness extends ComponentHarness {
+//   static hostSelector = 'app-ocx-no-help-item'
 
-  getContentSpan = this.locatorFor(SpanHarness.with({ id: 'no-help-item-content' }))
-  getHintSpan = this.locatorForOptional(SpanHarness.with({ id: 'no-help-item-hint' }))
-  getHintArticleSpan = this.locatorForOptional(SpanHarness.with({ id: 'no-help-item-hint-article-id' }))
+//   getContentSpan = this.locatorFor(SpanHarness.with({ id: 'no-help-item-content' }))
+//   getHintSpan = this.locatorForOptional(SpanHarness.with({ id: 'no-help-item-hint' }))
+//   getHintArticleSpan = this.locatorForOptional(SpanHarness.with({ id: 'no-help-item-hint-article-id' }))
 
-  async getContent(): Promise<string> {
-    return await (await this.getContentSpan()).getText()
-  }
+//   async getContent(): Promise<string> {
+//     return await (await this.getContentSpan()).getText()
+//   }
 
-  async getHintTitle(): Promise<string | undefined> {
-    return await (await this.getHintSpan())?.getText()
-  }
+//   async getHintTitle(): Promise<string | undefined> {
+//     return await (await this.getHintSpan())?.getText()
+//   }
 
-  async getArticleId(): Promise<string | undefined> {
-    return await (await this.getHintArticleSpan())?.getText()
-  }
-}
+//   async getArticleId(): Promise<string | undefined> {
+//     return await (await this.getHintArticleSpan())?.getText()
+//   }
+// }
