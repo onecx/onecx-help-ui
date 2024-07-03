466c57e4cc217595cf3195a7511b61ee2ee00ddf
Christian Badura <christian.badura@capgemini.com>
2024-07-03 12:13:38 +0200

    fix: show help

--- src/app/remotes/show-help/show-help.component.bootstrap.ts
+++ src/app/remotes/show-help/show-help.component.bootstrap.ts
@@ -1,29 +1,12 @@
-import { HttpClientModule, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { importProvidersFrom } from '@angular/core'
 import { OneCXShowHelpComponent } from './show-help.component'
 import { bootstrapRemoteComponent } from '@onecx/angular-webcomponents'
 import { AngularAuthModule } from '@onecx/angular-auth'
 import { environment } from 'src/environments/environment'
-import { NoHelpItemComponent, PortalCoreModule } from '@onecx/portal-integration-angular'
-import { CommonModule } from '@angular/common'
-import { TranslateModule } from '@ngx-translate/core'
-import { SharedModule } from 'primeng/api'
-import { DynamicDialogModule } from 'primeng/dynamicdialog'
-import { RippleModule } from 'primeng/ripple'
-import { TooltipModule } from 'primeng/tooltip'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 bootstrapRemoteComponent(OneCXShowHelpComponent, 'ocx-show-help-component', environment.production, [
   provideHttpClient(withInterceptorsFromDi()),
-  importProvidersFrom(
-    AngularAuthModule,
-    PortalCoreModule,
-    CommonModule,
-    HttpClientModule,
-    RippleModule,
-    TooltipModule,
-    DynamicDialogModule,
-    NoHelpItemComponent,
-    TranslateModule,
-    SharedModule
-  )
+  importProvidersFrom(AngularAuthModule, BrowserAnimationsModule)
 ])
