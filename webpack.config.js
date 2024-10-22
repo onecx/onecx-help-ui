94162abce0fe9279959e5cf07000efe0f2215829
Henry Täschner <129834483+HenryT-CG@users.noreply.github.com>
2024-10-22 16:16:34 +0200

    fix: added platform-browser to webpack config (#232)

--- webpack.config.js
+++ webpack.config.js
@@ -11,23 +11,13 @@ const config = withModuleFederationPlugin({
   },
   shared: share({
     '@angular/core': { requiredVersion: 'auto', includeSecondaries: true },
-    '@angular/forms': {
-      requiredVersion: 'auto',
-      includeSecondaries: true,
-      eager: false
-    },
-    '@angular/common': {
-      requiredVersion: 'auto',
-      includeSecondaries: {
-        skip: ['@angular/common/http/testing']
-      }
-    },
-    '@angular/common/http': {
-      requiredVersion: 'auto',
-      includeSecondaries: true
-    },
+    '@angular/forms': { requiredVersion: 'auto', includeSecondaries: true, eager: false },
+    '@angular/common': { requiredVersion: 'auto', includeSecondaries: { skip: ['@angular/common/http/testing'] } },
+    '@angular/common/http': { requiredVersion: 'auto', includeSecondaries: true },
     '@angular/router': { requiredVersion: 'auto', includeSecondaries: true },
+    '@angular/platform-browser': { requiredVersion: 'auto', includeSecondaries: true },
     rxjs: { requiredVersion: 'auto', includeSecondaries: true },
+    primeng: { requiredVersion: 'auto', includeSecondaries: true },
     '@ngx-translate/core': { requiredVersion: 'auto' },
     '@onecx/accelerator': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/angular-accelerator': { requiredVersion: 'auto', includeSecondaries: true },
@@ -39,30 +29,18 @@ const config = withModuleFederationPlugin({
     '@onecx/integration-interface': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/keycloak-auth': { requiredVersion: 'auto', includeSecondaries: true },
     '@onecx/portal-integration-angular': { requiredVersion: 'auto', includeSecondaries: true },
-    '@onecx/portal-layout-styles': { requiredVersion: 'auto', includeSecondaries: true },
-    primeng: { requiredVersion: 'auto', includeSecondaries: true }
+    '@onecx/portal-layout-styles': { requiredVersion: 'auto', includeSecondaries: true }
   }),
   sharedMappings: ['@onecx/portal-integration-angular']
 })
-config.devServer = {
-  allowedHosts: 'all'
-}
+config.devServer = { allowedHosts: 'all' }
 
 const plugins = config.plugins.filter((plugin) => !(plugin instanceof ModifyEntryPlugin))
 
 module.exports = {
   ...config,
   plugins,
-  output: {
-    uniqueName: 'onecx-help-ui',
-    publicPath: 'auto'
-  },
-  experiments: {
-    ...config.experiments,
-    topLevelAwait: true
-  },
-  optimization: {
-    runtimeChunk: false,
-    splitChunks: false
-  }
+  output: { uniqueName: 'onecx-help-ui', publicPath: 'auto' },
+  experiments: { ...config.experiments, topLevelAwait: true },
+  optimization: { runtimeChunk: false, splitChunks: false }
 }
