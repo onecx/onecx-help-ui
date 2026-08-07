ae9320ce32954b55bf3fe81149ec66fb061c2f0f
Henry Taeschner <github@henry-taeschner.de>
2026-08-07 19:35:12 +0200

    fix: using real PrimeNG buttons in topbar

--- src/app/remotes/help-item-editor/help-item-editor.harness.ts
+++ src/app/remotes/help-item-editor/help-item-editor.harness.ts
@@ -3,10 +3,12 @@ import { ComponentHarness } from '@angular/cdk/testing'
 export class OneCXHelpItemEditorHarness extends ComponentHarness {
   static readonly hostSelector = 'app-ocx-help-item-editor'
 
-  getHelpEditorButton = this.locatorForOptional('#ocx_topbar_action_edit_help_item')
+  // PrimeNG buttons embed real HTML buttons, which really are the ones that should be clicked
+  getHelpEditorPButton = this.locatorForOptional('#ocx_topbar_action_edit_help_item')
+  getHelpEditorButton = this.locatorForOptional('#ocx_topbar_action_edit_help_item button')
 
   async getHelpEditorButtonId(): Promise<string | null | undefined> {
-    return await (await this.getHelpEditorButton())?.getAttribute('id')
+    return await (await this.getHelpEditorPButton())?.getAttribute('id')
   }
 
   async clickHelpEditorButton() {
--- src/app/remotes/show-help/show-help.harness.ts
+++ src/app/remotes/show-help/show-help.harness.ts
@@ -3,10 +3,12 @@ import { ComponentHarness } from '@angular/cdk/testing'
 export class OneCXShowHelpHarness extends ComponentHarness {
   static readonly hostSelector = 'app-ocx-show-help'
 
-  getShowHelpButton = this.locatorForOptional('#ocx_topbar_action_show_help_item')
+  // PrimeNG buttons embed real HTML buttons, which really are the ones that should be clicked
+  getShowHelpButton = this.locatorForOptional('#ocx_topbar_action_show_help_item button')
+  getShowHelpPButton = this.locatorForOptional('#ocx_topbar_action_show_help_item')
 
   async getShowHelpButtonId(): Promise<string | null | undefined> {
-    return await (await this.getShowHelpButton())?.getAttribute('id')
+    return await (await this.getShowHelpPButton())?.getAttribute('id')
   }
 
   async onClickShowHelpButton() {
